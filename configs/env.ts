01ac69f713c3df459939b06ec24770bfd56a944c
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-12-10 10:18:30 +0530

    refactor: (config/env.ts) refactored for reliability and modularity

--- configs/env.ts
+++ configs/env.ts
@@ -1,9 +1,23 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const config = {
-    port: process.env.ONE_AUTH_SERVER_PORT,
-    DB_Url: process.env.ONE_AUTH_DATABASE_SERVER_URL,
+interface Config{
+    port: number;
+    databaseUrl: string;
+}
+
+const requireEnv=function (value: string) {
+    const ENV_VALUE = process.env[value];
+    if (!ENV_VALUE) {
+        console.log(`CONFIGURATION ERROR : ${value} is not defined in environment`);
+        return
+    }
+    return ENV_VALUE;
+}
+
+const config: Config={
+    port: Number(requireEnv("ONE_AUTH_SERVER_URL")),
+    databaseUrl: requireEnv("ONE_AUTH_DATABASE_SERVER_URL")!
 }
 
 export default config;
