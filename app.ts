91bdeb69cfc4e4d4adc1aed23d74b5a7ad884ba9
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-12-10 14:20:26 +0530

    feat: (middleware) Added requestLogger middleware for development

--- app.ts
+++ app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import errorHandler from "./middlewares/errorHandler";
+import requestLogger from "./middlewares/requestLogger";
 
 const app = express();
 app.use(express.json());
+app.use(requestLogger);
 app.use(errorHandler);
 
 export default app;
