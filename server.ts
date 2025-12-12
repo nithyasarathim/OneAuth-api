7ca1094f71e25088ace298c45f4ea7550242f2f3
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-12-12 11:53:33 +0530

    integration :  (server.ts) redis integrated with main server

--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
 import app from "./app.ts";
 import config from "./configs/env.ts";
-import connectDB from "./configs/db.ts";
+import connectDB from "./configs/database.ts";
+import { connectCache } from "./configs/cache.ts";
 
 const startServer = async () => {
   await connectDB();
+  await connectCache();
   app.listen(config.port, () => {
     console.log(
       `One Auth Server is running on http://localhost:${config.port}`
