c440dd22589ba11d026663cb08d5f1ed37177c29
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-12-20 19:22:22 +0530

    add : (user.endpoint) PATCH /users/me endpoint added for updating user profile with selective fields

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -5,18 +5,9 @@ import { redis } from '../configs/cache';
 
 const fetchPersonalData = async (req: Request, res: Response, next:NextFunction): Promise<void> => {
     try {
-        const sessionToken = req.cookies?.session_token;
-        if (!sessionToken) {
-            throw new ApiError("Session is not established", 401);
-        }
-        const userId = await redis.get(`session:${sessionToken}`);
-        if (!userId) {
-            redis.del(`session:${sessionToken}`);
-            throw new ApiError("Invalid or expired session", 401);
-        }
+        const userId = req.userId;
         const user = await UserAccount.findById(userId).select("-password");
         if (!user) {
-            redis.del(`session:${sessionToken}`);
             throw new ApiError("Invalid user token", 401);
         }
         res.status(200).json({
@@ -29,6 +20,56 @@ const fetchPersonalData = async (req: Request, res: Response, next:NextFunction)
     }
 }
 
+const updatePersonalData = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const userId = req.userId;
+        if (!userId) {
+            throw new ApiError("Unauthorized request", 401);
+        }
+        const allowedFields = [
+            "username",
+            "department",
+            "skills",
+            "linkedinUrl",
+            "githubUrl",
+            "description",
+            "isAvailable",
+        ];
+        const normalizedData: Record<string, any> = {};
+        for (const field of allowedFields) {
+            if (typeof req.body[field] === 'string') {
+                normalizedData[field] = req.body[field].trim();
+            }else if (req.body[field] !== undefined) {
+                normalizedData[field] = req.body[field];
+            }
+        }
+        if (Object.keys(normalizedData).length === 0) {
+            throw new ApiError("No valid fields to update", 400);
+        }
+        const updatedUser = await UserAccount.findByIdAndUpdate(
+            userId,
+            {
+                $set: normalizedData
+            },
+            {
+                new: true,
+                runValidators: true
+            }).select(
+                "username department skills linkedinUrl githubUrl description isAvailable profilePicture resumeUrl"
+            );
+        if (!updatedUser) {
+            throw new ApiError("No user found", 404);
+        }
+        res.status(200).json({
+            success: true,
+            user: updatedUser,
+        })
+    } catch (err) {
+        next(err);
+    }
+};
+
 export {
-    fetchPersonalData
+    fetchPersonalData,
+    updatePersonalData
 }
--- routers/user.route.ts
+++ routers/user.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import limitRate from "../middlewares/rateLimiter";
-import { fetchPersonalData } from "../controllers/user.controller";
+import { fetchPersonalData, updatePersonalData } from "../controllers/user.controller";
+import authorize from "../middlewares/authenticator";
 
 const userRouter = Router();
 
-userRouter.get("/me", limitRate(60), fetchPersonalData);
+userRouter.get("/me", limitRate(60), authorize, fetchPersonalData);
+userRouter.patch("/me", limitRate(10), authorize, updatePersonalData);
 
 export default userRouter;
